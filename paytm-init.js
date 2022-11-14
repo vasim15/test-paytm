e7717da479a58d6a97e50dbacc64738fd8c14721
Vasim Aalam <vasimzgreen@gmail.com>
2022-11-14 20:57:11 +0530

    second fff

--- paytm-init.js
+++ paytm-init.js
@@ -67,6 +67,26 @@ export default (req, res, next) => {
                 ...response,
                 ...req.body,
                 cbUrl: cbUrl,
+                htmlBody: `<html>
+                       <head>
+                          <title>Show Payment Page</title>
+                       </head>
+                       <body>
+                          <center>
+                             <h1>Please do not refresh this page...</h1>
+                          </center>
+                          <form method="post" action="https://securegw-stage.paytm.in/theia/api/v1/showPaymentPage?mid=${mig}&orderId=${orderId}" name="paytm">
+                             <table border="1">
+                                <tbody>
+                                    <input type="hidden"  name="mid" value="${mig}">
+                                    <input type="hidden" name="orderId" value="${orderId}">
+                                    <input type="hidden" name="txnToken" value="${response.body.txnToken}">
+                                </tbody>
+                             </table>
+                             <script type="text/javascript"> document.paytm.submit(); </script>
+                          </form>
+                       </body>
+                  </html>`,
               },
             });
           });
