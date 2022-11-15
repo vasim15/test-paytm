e60152fb844f899a145e33eb01985baf65f50e25
Vasim Aalam <vasimzgreen@gmail.com>
2022-11-15 14:16:34 +0530

    p ptff

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ import express from "express";
 import cors from "cors";
 import paytmInti from "./paytm-init";
 import paytmVrfy from "./paytm-vrfy";
+import prodPaytmInti from "./prod-paytm-init";
+import prodPaytmVrfy from "./prod-paytm-vrfy";
+
 
 const app = express();
 const PORT = process.env.PORT || 4415;
@@ -12,6 +15,9 @@ app.use(express.json());
 app.get("/", (rq, rs) => rs.json({ message: "good attempt bhai.." }));
 app.post("/payment-initiate", paytmInti);
 app.post("/payment-verify", paytmVrfy);
+
+app.post("/king-rathode-payment-initiate", prodPaytmInti);
+app.post("/king-rathode-payment-verify", prodPaytmVrfy);
 
 app.use((err, req, res, next) => console.log(err));
 app.listen(PORT, () => {
